ab874c3002217e1b6b335e992e51b55a550a4e40
Yair Biber <yair.biber@ourcrowd.com>
2022-05-25 00:30:08 +0300

    fic cards

--- src/components/VehicleItem.js
+++ src/components/VehicleItem.js
@@ -1,42 +1,27 @@
 import React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import VehicleItemModal from './VehicleItemModal';
-
-const MediaCard = ({data, deleteVehicle, userName}) => {
-  return (
-    <Card sx={{ maxWidth: 345 }} >
-      <button className='open-modal-btn' data-target={`simpleModal_${data.id}`} data-toggle="modal">...</button>
-      <CardMedia
-        component="img"
-        height="200"
-        image={data.imagesUrls[0] || '/no-img.jpg'}
-        alt=""
-      />
-     {data.renter === userName && <Button className='delete-vehicle' onClick={() => {deleteVehicle(data.id)}}>&#128465;</Button>}
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {data.type}
-        </Typography>
-        <div className='short-desc'>{data.desc}</div>
-        <div className='stars'>{Array(data.rateAvg).fill().map(()=> <i key={Math.random()} className='fa fa-star fa-2x'></i>)}</div>
-      </CardContent>
-      <CardContent className='small-card-footer'>
-        <div className='price'>{data.price} $</div>
-        <div className='location'>{data.placeName}</div>
-        <div className='distance'>{data.distance?.toFixed(1)} km</div>
-        </CardContent>
-    </Card>
-  );
-}
 
 const VehicleItem = ({data, deleteVehicle, userName}) => {
     return<div className='vehicle-card'>
       <div className='small-vehicle-item'>
-      { MediaCard({data, deleteVehicle, userName})}
+        <div className='vehicle-item-small' >
+        <button className='open-modal-btn' data-target={`simpleModal_${data.id}`} data-toggle="modal">...</button>
+        <div className='cover-img'><img src={data.imagesUrls[0] || '/no-img.jpg'} alt=''/></div>
+      {data.renter === userName && <Button className='delete-vehicle' onClick={() => {deleteVehicle(data.id)}}>&#128465;</Button>}
+        <div className='card-center-row'>
+        <div className='type'>
+            {data.type}
+        </div>
+          <div className='short-desc'>{data.desc}</div>
+          <div className='stars'>{Array(data.rateAvg).fill().map(()=> <i key={Math.random()} className='fa fa-star fa-2x'></i>)}</div>
+        <div className='small-card-footer'>
+        </div>
+          <div className='price'>{data.price} $</div>
+          <div className='location'>{data.placeName}</div>
+          <div className='distance'>{data.distance?.toFixed(1)} km</div>
+          </div>
+      </div>
       </div>
       {VehicleItemModal(data, userName)}
     </div>; 
