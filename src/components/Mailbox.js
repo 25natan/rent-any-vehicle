901aaa90184302a956d0a781cd595cec44858aa0
Yair Biber <yair.biber@ourcrowd.com>
2022-05-24 22:06:51 +0300

    a

--- src/components/Mailbox.js
+++ src/components/Mailbox.js
@@ -1,13 +1,15 @@
-import { collection, doc, getDoc, getDocs, orderBy, query, where } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, orderBy, query, setDoc, Timestamp, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import { v4 } from 'uuid';
 import { db } from '../firebase-config';
 
 const Mailbox = ({isAuth, userName}) => {
     const [inbox, setInbox] = useState([]);
     const [outbox, setOutbox] = useState([]);
     const [boxToDisplay, setBoxToDisplay] = useState('in');
+    const [msgToReply, setMsgToReply] = useState('');
     let navigate = useNavigate();
 
     const getMessages = async () => {
@@ -31,15 +33,35 @@ const Mailbox = ({isAuth, userName}) => {
             }
       };
 
+      const sendMsg = async ({to, ind}) => {
+        try{
+            const userDocRef = doc(db, 'mail-box', v4());
+            const now = Timestamp.now()
+            await setDoc(userDocRef, {message: msgToReply, from: userName, to, /*vehicle: data.id,*/ date: now});
+            document.getElementById(`reply-msg-inbox-${ind}`).style.display = 'none';
+            document.getElementById(`approve-${ind}`).style.display = 'flex';
+            } catch (error) {
+              console.log(error);
+            }
+      }
+
       const getInbox = () => {
-          return inbox.map(msg => <div className='mail-card' key={msg}>
+          return inbox.map((msg, ind) => <div className='mail-card' key={ind}>
           <div className='msg-card'>
               <div className='msg-card-header'>
                  <div className='from'> {msg.from}</div>
                  <div className='msg-date'>  {msg.date}</div>
               </div>
               <div className='msg-card-body'>{msg.message}</div>
-          </div>
+              <div className='msg-card-footer'>
+              <button onClick={() => document.getElementById(`reply-msg-inbox-${ind}`).style.display = 'flex'}>Reply</button>
+              <div className='reply-msg' id={`reply-msg-inbox-${ind}`}>
+              <textarea onChange={(e)=> setMsgToReply(e.target.value)}></textarea>
+              <button onClick={() => sendMsg({to: msg.from, ind})}>Send</button>
+              </div> 
+              <div className='approve' id={`approve-${ind}`}>message sent</div>
+              </div>         
+            </div>
       </div>);
       };
 
