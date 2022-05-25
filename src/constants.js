070415c382cbb40f8ba18064799bfe7965e93ac7
Yair Biber <yair.biber@ourcrowd.com>
2022-05-25 17:55:43 +0300

    a

--- src/constants.js
+++ src/constants.js
@@ -2,27 +2,26 @@
   const vihecleTypes = [
         "Private Car",
         "Van",
-        "Taxi",
-        "Bus",
         "Ambulance",
         "Skateboard",
         "Baby carriage",
         "Bicycle",
+        "Rollerblades",
+        "Electric bike",
         "Mountain bike",
         "Scooter",
         "Motorcycle",
-        "Fire engine",
         "Crane",
         "Forklift",
         "Tractor",
         "Recycling truck",
-        "Cement mixer",
         "Dump truck",
         "Helicopter",
         "Airplane",
         "Carriage",
-        "Rowboat",
         "Boat",
+        "Taxi",
+        "Bus"
       ];
     const sortTypes = ['price', 'rate', 'location']
 
